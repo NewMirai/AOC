579f27fb68d5ea917af50a59c1a0fb16705f2bd3
Alain Anghelidi <alainanghelidi@gmail.com>
2021-11-07 18:26:08 +0100

    [FEAT] Added sample solution file

---
+++ 0000/day00/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ReadInput of the problem
+func ReadInput(f string) string {
+	data, err := os.ReadFile(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+// solve problem
+func solve(s string) int {
+	var total int
+	var numbers []int
+	lines := strings.Split(strings.TrimSpace(s), "\n")
+	// for i, line := range lines {
+	// 	fmt.Println("Line", i, ": ", line)
+	// }
+	for _, line := range lines {
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+// main function
+func main() {
+	problem_input := ReadInput("input.txt")
+	fmt.Println(solve(problem_input))
+}
