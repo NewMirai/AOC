30dd8e2269e813eb86b8acbcd681bc3bffdbaec8
Alain Anghelidi <aanghelidi@fieldbox.ai>
2021-12-13 22:20:03 +0100

    [FEAT] Added day03

--- 2021/day03/main.go
+++ 2021/day03/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
 )
@@ -18,14 +17,11 @@ func ReadInput(f string) string {
 	return string(data)
 }
 
-func RemoveIndex(s [][]rune, index int) [][]rune {
-	return append(s[:index], s[index+1:]...)
-}
-
 // Solve problem part 1
 func Solve1(s *string) (total int64) {
 	lines := strings.Split(strings.TrimSpace(*s), "\n")
 	Grid := make([][]rune, len(lines))
+	// Populate the grid
 	for i, line := range lines {
 		numbers := make([]rune, len(line))
 		for idx, num := range line {
@@ -36,8 +32,8 @@ func Solve1(s *string) (total int64) {
 	lcol := len(Grid[0])
 	lgr := make([]string, lcol)
 	for j := 0; j < lcol; j++ {
-		c0 := 0
-		c1 := 0
+		var c0 int
+		var c1 int
 		var mc string
 		for i := 0; i < len(lines); i++ {
 			el := Grid[i][j]
@@ -63,22 +59,21 @@ func Solve1(s *string) (total int64) {
 			ser += "0"
 		}
 	}
-	gr, _ := strconv.ParseInt(sgr, 2, 64)
-	er, _ := strconv.ParseInt(ser, 2, 64)
+	gr, _ := strconv.ParseInt(sgr, 2, 8)
+	er, _ := strconv.ParseInt(ser, 2, 8)
 	total = gr * er
 	return total
-}
-
-func remove(slice []string, s int) []string {
-	return append(slice[:s], slice[s+1:]...)
 }
 
 // Solve problem part 2
 func Solve2(s *string) (total int64) {
 	lines := strings.Split(strings.TrimSpace(*s), "\n")
+	lines2 := make([]string, len(lines))
+	copyN := copy(lines2, lines)
+	fmt.Println(copyN)
 	Grid := make([][]rune, len(lines))
-	numbers := make([]rune, len(lines[0]))
 	for i, line := range lines {
+		numbers := make([]rune, len(lines[0]))
 		for idx, num := range line {
 			numbers[idx] = num
 		}
@@ -87,36 +82,74 @@ func Solve2(s *string) (total int64) {
 	lcol := len(Grid[0])
 	for len(lines) > 1 {
 		for j := 0; j < lcol; j++ {
+			linesFiltered := make([]string, 0)
 			c0 := 0
 			c1 := 0
 			var mc string
 			for i := 0; i < len(lines); i++ {
-				el := Grid[i][j]
-				if el == '0' {
+				if lines[i][j] == '0' {
 					c0++
 				} else {
 					c1++
 				}
-				if c1 > c0 {
+				if c1 >= c0 {
 					mc = "1"
 				} else {
 					mc = "0"
 				}
 			}
-			for i, line := range lines {
+			for _, line := range lines {
 				if string(line[j]) == mc {
-					runtime.Breakpoint()
-					lines = remove(lines, i)
+					linesFiltered = append(linesFiltered, line)
 				}
 			}
+			// update
+			lines = linesFiltered
 		}
 	}
+
+loop:
+	for len(lines2) > 1 {
+		for j := 0; j < lcol; j++ {
+			linesFiltered2 := make([]string, 0)
+			c0 := 0
+			c1 := 0
+			var lc string
+			for i := 0; i < len(lines2); i++ {
+				if lines2[i][j] == '0' {
+					c0++
+				} else {
+					c1++
+				}
+				if c1 >= c0 {
+					lc = "1"
+				} else {
+					lc = "0"
+				}
+			}
+			for _, line := range lines2 {
+				if len(lines2) == 1 {
+					break loop
+				}
+				if string(line[j]) != lc {
+					linesFiltered2 = append(linesFiltered2, line)
+				}
+			}
+			// update
+			lines2 = linesFiltered2
+		}
+	}
+	sgr := lines[0]
+	ser := lines2[0]
+	gr, _ := strconv.ParseInt(sgr, 2, 64)
+	er, _ := strconv.ParseInt(ser, 2, 64)
+	total = gr * er
 	return total
 }
 
 // main function
 func main() {
-	problem_input := ReadInput("input.test")
+	problem_input := ReadInput("input.txt")
 	fmt.Println(Solve1(&problem_input))
 	fmt.Println(Solve2(&problem_input))
 }
