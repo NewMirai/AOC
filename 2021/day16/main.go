6a95b2158b08fb8bb68a439ca1ccb7eaced50d86
Alain Anghelidi <alainanghelidi@gmail.com>
2021-12-16 09:02:42 +0100

    [WIP] wip day 16

---
+++ 2021/day16/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func ReadInput(f string) string {
+	data, _ := os.ReadFile(f)
+	return string(data)
+}
+
+func DecodeHex(s string, total *int) {
+	var (
+		packetVersion int
+		typeID        int
+	)
+	fmt.Sscanf(s[0:3], "%b", &packetVersion)
+	fmt.Sscanf(s[3:6], "%b", &typeID)
+	packetType := CheckID(typeID)
+	if packetType == "literal" {
+		DecodeLiteral(s, packetVersion, typeID, total)
+	} else {
+		DecodeOperator(s, packetVersion, typeID, total)
+	}
+}
+
+func DecodeLiteral(s string, version int, typeID int, total *int) {
+	var value int
+	A, B, C := s[6:11], s[11:16], s[16:21]
+	n1, n2, n3 := A[1:], B[1:], C[1:]
+	tmp := n1 + n2 + n3
+	fmt.Sscanf(tmp, "%b", &value)
+	*total += value
+}
+
+func DecodeOperator(s string, version int, typeID int, total *int) {
+	var subLength int
+	lBitNum := GetLengthBit(s[6:7])
+	subLengthS := s[7 : 7+lBitNum]
+	fmt.Sscanf(subLengthS, "%b", &subLength)
+	switch typeID {
+	case 6:
+		sCount := 0
+
+	}
+	runtime.Breakpoint()
+}
+
+func GetLengthBit(ltID string) int {
+	cTable := map[string]int{
+		"0": 15,
+	}
+	return cTable[ltID]
+}
+
+func CheckID(id int) string {
+	switch id {
+	case 4:
+		return "literal"
+	default:
+		return "operator"
+	}
+}
+
+func IsSmall(s string) bool {
+	if s[0] == '0' {
+		return true
+	}
+	return false
+}
+
+func Solve(s *string) (total int) {
+	raw := strings.TrimSpace(*s)
+	hex2Binary := map[string]string{
+		"0":  "0000",
+		"1":  "0001",
+		"2":  "0010",
+		"3":  "0011",
+		"4 ": "0100",
+		"5":  "0101",
+		"6":  "0110",
+		"7":  "0111",
+		"8":  "1000",
+		"9":  "1001",
+		"A":  "1010",
+		"B":  "1011",
+		"C":  "1100",
+		"D":  "1101",
+		"E":  "1110",
+		"F":  "1111",
+	}
+	var rawDecoded string
+	for _, c := range raw {
+		rawDecoded += hex2Binary[string(c)]
+	}
+	DecodeHex(rawDecoded, &total)
+	return total
+}
+
+func main() {
+	problem_input := ReadInput("input.test2")
+	fmt.Println(Solve(&problem_input))
+}
