7b31fbf8d3b9675ad5cfb91aefef9fb398f7186d
Alain Anghelidi <aanghelidi@fieldbox.ai>
2021-12-17 14:42:54 +0100

    [FEAT] added day 15

--- 2021/day15/main.go
+++ 2021/day15/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,6 +11,64 @@ import (
 func ReadInput(f string) string {
 	data, _ := os.ReadFile(f)
 	return string(data)
+}
+
+type Coordinate struct {
+	x int
+	y int
+}
+
+func (c *Coordinate) GetAdjacentCoordinates() []*Coordinate {
+	adj := make([]*Coordinate, 4)
+	adj[0] = &Coordinate{x: c.x, y: c.y - 1}
+	adj[1] = &Coordinate{x: c.x + 1, y: c.y}
+	adj[2] = &Coordinate{x: c.x, y: c.y + 1}
+	adj[3] = &Coordinate{x: c.x - 1, y: c.y}
+	return adj
+}
+
+type Vertex struct {
+	cost  int
+	pos   *Coordinate
+	index int
+}
+
+type VertexQueue []*Vertex
+
+func (vq VertexQueue) Len() int { return len(vq) }
+
+func (vq VertexQueue) Less(i, j int) bool {
+	// Return the lowest cost
+	return vq[i].cost < vq[j].cost
+}
+
+func (vq VertexQueue) Swap(i, j int) {
+	vq[i], vq[j] = vq[j], vq[i]
+	vq[i].index = i
+	vq[j].index = j
+}
+
+func (vq *VertexQueue) Push(x interface{}) {
+	n := len(*vq)
+	vertex := x.(*Vertex)
+	vertex.index = n
+	*vq = append(*vq, vertex)
+}
+
+func (vq *VertexQueue) Pop() interface{} {
+	old := *vq
+	n := len(*vq)
+	vertex := old[n-1]
+	old[n-1] = nil    // avoid memory leak
+	vertex.index = -1 // for safety
+	*vq = old[0 : n-1]
+	return vertex
+}
+
+func (vq *VertexQueue) update(vertex *Vertex, pos *Coordinate, cost int) {
+	vertex.pos = &Coordinate{x: pos.x, y: pos.y}
+	vertex.cost = cost
+	heap.Fix(vq, vertex.index)
 }
 
 func Min(x int, y int) int {
@@ -40,6 +99,56 @@ func MinPathSum(grid [][]int, H int, W int) int {
 	return dp[H-1][W-1] - dp[0][0]
 }
 
+func MinPathSum2(grid [][]int, H int, W int) int {
+	total := 0
+	vq := make(VertexQueue, 0)
+	start := &Coordinate{x: 0, y: 0}
+	target := &Coordinate{x: W - 1, y: H - 1}
+	heap.Init(&vq)
+	initV := &Vertex{cost: 0, pos: start}
+	heap.Push(&vq, initV)
+	visited := map[Coordinate]struct{}{
+		*start: struct{}{},
+	}
+	for {
+		v := heap.Pop(&vq).(*Vertex)
+		cost, pos := v.cost, v.pos
+		if pos.x == target.x && pos.y == target.y {
+			total = cost
+			break
+		}
+		for _, apos := range pos.GetAdjacentCoordinates() {
+			_, ok := visited[*apos]
+			if 0 <= apos.y && apos.y < H && 0 <= apos.x && apos.x < W && !ok {
+				newV := &Vertex{
+					cost: grid[apos.y][apos.x] + cost,
+					pos:  apos,
+				}
+				heap.Push(&vq, newV)
+				visited[*apos] = struct{}{}
+			}
+		}
+	}
+	return total
+}
+
+func Repeat(grid [][]int) [][]int {
+	H := len(grid)
+	W := len(grid[0])
+	newGrid := make([][]int, H)
+	for i := 0; i < H; i++ {
+		newGrid[i] = make([]int, W)
+		for j := 0; j < W; j++ {
+			newValue := grid[i][j] + 1
+			if newValue > 9 {
+				newValue = 1
+			}
+			newGrid[i][j] = newValue
+		}
+	}
+	return newGrid
+}
+
 func Solve(s *string) (total int) {
 	lines := strings.Split(strings.TrimSpace(*s), "\n")
 	H := len(lines)
@@ -52,11 +161,84 @@ func Solve(s *string) (total int) {
 			grid[i][j] = cost
 		}
 	}
-	total = MinPathSum(grid, H, W)
+	// part 1
+	fmt.Println("Part 1 using dp: ", MinPathSum(grid, H, W))
+
+	r2 := Repeat(grid)
+	r3 := Repeat(r2)
+	r4 := Repeat(r3)
+	r5 := Repeat(r4)
+	bigGrid := make([][]int, H)
+	for i := 0; i < H; i++ {
+		bigGrid[i] = make([]int, 5*W)
+		for j := 0; j < W; j++ {
+			for k := 0; k < 5; k++ {
+				switch k {
+				case 0:
+					bigGrid[i][j+H*k] = grid[i][j]
+				case 1:
+					bigGrid[i][j+H*k] = r2[i][j]
+				case 2:
+					bigGrid[i][j+H*k] = r3[i][j]
+				case 3:
+					bigGrid[i][j+H*k] = r4[i][j]
+				case 4:
+					bigGrid[i][j+H*k] = r5[i][j]
+				}
+			}
+		}
+	}
+	r2c := Repeat(bigGrid)
+	r3c := Repeat(r2c)
+	r4c := Repeat(r3c)
+	r5c := Repeat(r4c)
+	bigGridComplete := make([][]int, 5*len(bigGrid))
+	for i := 0; i < 5*H; i++ {
+		bigGridComplete[i] = make([]int, len(bigGrid[0]))
+	}
+	for k := 0; k < 5; k++ {
+		for i := 0; i < H; i++ {
+			for j := 0; j < W; j++ {
+				bigGridComplete[i][j+H*k] = bigGrid[i][j+H*k]
+			}
+		}
+	}
+	for k := 0; k < 5; k++ {
+		for i := 0; i < H; i++ {
+			for j := 0; j < W; j++ {
+				bigGridComplete[i+H][j+H*k] = r2c[i][j+H*k]
+			}
+		}
+	}
+	for k := 0; k < 5; k++ {
+		for i := 0; i < H; i++ {
+			for j := 0; j < W; j++ {
+				bigGridComplete[i+2*H][j+H*k] = r3c[i][j+H*k]
+			}
+		}
+	}
+	for k := 0; k < 5; k++ {
+		for i := 0; i < H; i++ {
+			for j := 0; j < W; j++ {
+				bigGridComplete[i+3*H][j+H*k] = r4c[i][j+H*k]
+			}
+		}
+	}
+	for k := 0; k < 5; k++ {
+		for i := 0; i < H; i++ {
+			for j := 0; j < W; j++ {
+				bigGridComplete[i+4*H][j+H*k] = r5c[i][j+H*k]
+			}
+		}
+	}
+	// part 2
+	H = len(bigGridComplete)
+	W = len(bigGridComplete[0])
+	total = MinPathSum2(bigGridComplete, H, W)
 	return total
 }
 
 func main() {
 	problem_input := ReadInput("input.txt")
-	fmt.Println(Solve(&problem_input))
+	fmt.Println("Part 2 using heap: ", Solve(&problem_input))
 }
--- 2021/day15/main_test.go
+++ 2021/day15/main_test.go
@@ -1,30 +1,16 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestDay00(t *testing.T) {
-	type TableTests struct {
-		input string
-		want  int
-	}
+var result int
 
-	// Write your test cases
-	tests := []TableTests{
-		{input: "1\n2\n", want: 3},
-		{input: "0\n2\n", want: 2},
-		{input: "3\n2\n", want: 5},
-		{input: "1\n\n", want: 1},
+func BenchmarkDay15(b *testing.B) {
+	var r int
+	problem_input := ReadInput("input.txt")
+	for n := 0; n < b.N; n++ {
+		r = Solve(&problem_input)
 	}
-
-	for _, tc := range tests {
-		problem_input := tc.input
-		got := Solve(&problem_input)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Fatalf("expected: %v, got: %v", tc.want, got)
-		}
-	}
-
+	result = r
 }
