0821c4dce5232d521d3795dbc8cecb82a6103834
Alain Anghelidi <alainanghelidi@gmail.com>
2021-12-21 17:24:10 +0100

    [WIP] day16

--- 2021/day16/main.go
+++ 2021/day16/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -12,104 +14,179 @@ func ReadInput(f string) string {
 	return string(data)
 }
 
-func readBinary(s string, start int, end int) int {
-	var number int
-	fmt.Sscanf(s[start:end], "%b", &number)
-	return number
+type Packet interface {
+	Decode()
+	SumVersion() int
 }
 
-func DecodeHex(s string, total *int) {
-	packetVersion := readBinary(s, 0, 3)
-	typeID := readBinary(s, 3, 6)
-	packetType := CheckID(typeID)
-	if packetType == "literal" {
-		DecodeLiteral(s, packetVersion, typeID, total)
-	} else {
-		DecodeOperator(s, packetVersion, typeID, total)
+type Header struct {
+	packetVersion int
+	typeID        int
+}
+
+// A single packet
+type LiteralPacket struct {
+	repr   string
+	header Header
+	value  int
+}
+
+func (lp *LiteralPacket) GetNumber() (number int) {
+	sRepr := lp.repr[6:]
+	if IsLastGroup(sRepr[0]) {
+		number = ReadBitNumber(sRepr[1:])
+		return
 	}
-}
-
-func DecodeLiteral(s string, version int, typeID int, total *int) {
-	var value int
-	A, B, C := s[6:11], s[11:16], s[16:21]
-	n1, n2, n3 := A[1:], B[1:], C[1:]
-	tmp := n1 + n2 + n3
-	fmt.Sscanf(tmp, "%b", &value)
-	*total += value
-}
-
-func DecodeOperator(s string, version int, typeID int, total *int) {
-	var subLength int
-	lBitNum := GetLengthBit(s[6:7])
-	subLengthS := s[7 : 7+lBitNum]
-	fmt.Sscanf(subLengthS, "%b", &subLength)
-	switch typeID {
-	case 6:
-		c := 0
-		for c < 2 {
-			switch c {
-			case 0:
-				packet := s[7+lBitNum : 7+lBitNum+11]
-				DecodeHex(packet, total)
-			}
-			c++
+	N := len(sRepr)
+	nParts := N / 5
+	parts := make([]string, nParts)
+	start, end := 0, 5
+	for i := 0; i < nParts; i++ {
+		switch i {
+		case nParts - 1:
+			parts[i] = sRepr[start+1 : start+1+4]
+		default:
+			parts[i] = sRepr[start+1 : end]
 		}
+		start += 5
+		end += 5
+	}
+	binStr := strings.Join(parts, "")
+	number = ReadBitNumber(binStr)
+	return
+}
+
+func (lp *LiteralPacket) Decode() {
+	value := lp.GetNumber()
+	lp.value = value
+}
+
+func (lp *LiteralPacket) SumVersion() int { return lp.header.packetVersion }
+
+// Contains 1 or more packets
+type OperatorPacket struct {
+	repr      string
+	header    Header
+	lenTypeID int
+	bitLen    int // total bit length of subpacket
+	nPackets  int
+	packets   []Packet
+}
+
+func (op *OperatorPacket) Decode() {
+	runtime.Breakpoint()
+	switch op.lenTypeID {
+	case 0:
+		op.bitLen = ReadBitNumber(op.repr[7 : 7+15])
+		subPackets := op.repr[7+15 : 7+15+op.bitLen]
+		op.nPackets = len(subPackets) / 11
+		start, end := 0, 11
+		packets := make([]Packet, op.nPackets)
+		for i := 0; i < op.nPackets; i++ {
+			switch i {
+			case op.nPackets - 1:
+				rawPacket := subPackets[start:]
+				p := bin2Packet(rawPacket)
+				packets[i] = p
+
+			default:
+				rawPacket := subPackets[start:end]
+				p := bin2Packet(rawPacket)
+				packets[i] = p
+			}
+			start += 11
+			end += 11
+		}
+		for _, packet := range packets {
+			packet.Decode()
+		}
+		op.packets = packets
+	case 1:
+		op.nPackets = ReadBitNumber(op.repr[7 : 7+11])
+		subPackets := op.repr[7+11:]
+		start, end := 0, 11
+		packets := make([]Packet, op.nPackets)
+		for i := 0; i < op.nPackets; i++ {
+			rawPacket := subPackets[start:end]
+			p := bin2Packet(rawPacket)
+			packets[i] = p
+			start += 11
+			end += 11
+		}
+		for _, packet := range packets {
+			packet.Decode()
+		}
+		op.packets = packets
 	}
 }
 
-func GetLengthBit(ltID string) int {
-	cTable := map[string]int{
-		"0": 15,
+func (op *OperatorPacket) SumVersion() (total int) {
+	for _, packet := range op.packets {
+		total = packet.SumVersion()
 	}
-	return cTable[ltID]
+	return
 }
 
-func CheckID(id int) string {
-	switch id {
-	case 4:
-		return "literal"
-	default:
-		return "operator"
+// Convert hex to binary
+// hex -> byte -> binary string
+func Hex2Binary(s string) (decoded string) {
+	byteArray, _ := hex.DecodeString(s)
+	binArray := make([]string, len(byteArray))
+	for i := range byteArray {
+		binArray[i] = fmt.Sprintf("%08b", byteArray[i])
 	}
+	decoded = strings.Join(binArray, "")
+	return
 }
 
-func IsSmall(s string) bool {
-	if s[0] == '0' {
+// Read bit number
+func ReadBitNumber(s string) (number int) {
+	fmt.Sscanf(s, "%b", &number)
+	return
+}
+
+// Check if last group
+func IsLastGroup(r byte) bool {
+	if r == '0' {
 		return true
 	}
 	return false
 }
 
+// Convert string to Packet partially initialize
+func bin2Packet(s string) (p Packet) {
+	packetVersion, typeID := s[0:3], s[3:6]
+	header := Header{
+		packetVersion: ReadBitNumber(packetVersion),
+		typeID:        ReadBitNumber(typeID),
+	}
+	switch ReadBitNumber(typeID) {
+	case 4:
+		p = &LiteralPacket{
+			repr:   s,
+			header: header,
+		}
+	default:
+		lenTypeID, _ := strconv.Atoi(string(s[6]))
+		p = &OperatorPacket{
+			repr:      s,
+			header:    header,
+			lenTypeID: lenTypeID,
+		}
+	}
+	return
+}
+
 func Solve(s *string) (total int) {
 	raw := strings.TrimSpace(*s)
-	hex2Binary := map[string]string{
-		"0":  "0000",
-		"1":  "0001",
-		"2":  "0010",
-		"3":  "0011",
-		"4 ": "0100",
-		"5":  "0101",
-		"6":  "0110",
-		"7":  "0111",
-		"8":  "1000",
-		"9":  "1001",
-		"A":  "1010",
-		"B":  "1011",
-		"C":  "1100",
-		"D":  "1101",
-		"E":  "1110",
-		"F":  "1111",
-	}
-	var rawDecoded string
-	for _, c := range raw {
-		rawDecoded += hex2Binary[string(c)]
-	}
-	runtime.Breakpoint()
-	DecodeHex(rawDecoded, &total)
+	rawDecoded := Hex2Binary(raw)
+	p := bin2Packet(rawDecoded)
+	p.Decode()
+	total = p.SumVersion()
 	return total
 }
 
 func main() {
-	problem_input := ReadInput("input.test2")
+	problem_input := ReadInput("input.test4")
 	fmt.Println(Solve(&problem_input))
 }
